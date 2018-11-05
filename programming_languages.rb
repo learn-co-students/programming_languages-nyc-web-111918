e6aca42d330043e72557f290179f08bd74ca3bd1
Connor Finnegan <connorfinnegan16@gmail.com>
2018-11-05 23:36:48 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,11 +4,17 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, data|
     data.each do |language_name, language_data|
-      new_hash[language_name] = language_data
-      new_hash[language_name][:style] = []
+      language_data.each do |attribute, value|
+        if new_hash[language_name] == nil 
+          new_hash[language_name] = {}
+        end
+      new_hash[language_name][:style] ||= []
       new_hash[language_name][:style] << style
+        if new_hash[language_name][attribute] == nil
+          new_hash[language_name][attribute] = value
+        end
+      end
     end
   end
- new_hash[:javascript][:style] << :oo 
 new_hash
 end
